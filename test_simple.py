ccdd9c1be00d5b15269aab09a58fbd00221654ee
Zilin Wang <wangzilin20078@gmail.com>
2023-02-02 22:19:29 -0500

    Adapt to newer versions of the package

--- test_simple.py
+++ test_simple.py
@@ -124,7 +124,7 @@ def test_simple(args):
             # Load image and preprocess
             input_image = pil.open(image_path).convert('RGB')
             original_width, original_height = input_image.size
-            input_image = input_image.resize((feed_width, feed_height), pil.LANCZOS)
+            input_image = input_image.resize((feed_width, feed_height), pil.Resampling.LANCZOS)
             input_image = transforms.ToTensor()(input_image).unsqueeze(0)
 
             # PREDICTION
