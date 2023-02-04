a9fd997895070e7ee4faedba871b8ecfeba59b9c
Zilin Wang <wangzilin20078@gmail.com>
2023-02-03 23:59:53 -0500

    Add youtube dataset class

---
+++ datasets/youtube_dataset.py
@@ -0,0 +1,49 @@
+# Copyright Niantic 2019. Patent Pending. All rights reserved.
+#
+# This software is licensed under the terms of the Monodepth2 licence
+# which allows for non-commercial use only, the full terms of which are made
+# available in the LICENSE file.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import skimage.transform
+import numpy as np
+import PIL.Image as pil
+
+from .mono_dataset import MonoDataset
+
+
+class YOUTUBEDataset(MonoDataset):
+    """Superclass for different types of KITTI dataset loaders
+    """
+    def __init__(self, *args, **kwargs):
+        super(YOUTUBEDataset, self).__init__(*args, **kwargs)
+
+        # NOTE: Make sure your intrinsics matrix is *normalized* by the original image size.
+        # To normalize you need to scale the first row by 1 / image_width and the second row
+        # by 1 / image_height. Monodepth2 assumes a principal point to be exactly centered.
+        # If your principal point is far from the center you might need to disable the horizontal
+        # flip augmentation.
+
+        ### Not needed for Youtube
+        self.K = np.array([[0.58, 0, 0.5, 0],
+                           [0, 1.92, 0.5, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]], dtype=np.float32)
+
+    def check_depth(self):
+        return False
+
+    def get_color(self, folder, frame_index, side, do_flip):
+        color = self.loader(self.get_image_path(folder, frame_index, side))
+
+        if do_flip:
+            color = color.transpose(pil.FLIP_LEFT_RIGHT)
+
+        return color
+
+    def get_image_path(self, folder, frame_index, side):
+        f_str = "{:05d}{}".format(frame_index, self.img_ext)
+        image_path = os.path.join(self.data_path, folder, f_str)
+        return image_path
